ef3475f7760a9fced89594c9fe704b6d998e4ae5
SrushtiGangurde <75629779+SrushtiGangurde@users.noreply.github.com>
2021-08-25 08:49:12 +0530

    deleted dashboard

--- Front End/src/app/app-routing.module.ts
+++ Front End/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-//import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminHomeComponent } from './AdminHomePage/admin-home/admin-home.component';
 import { LoginComponent } from './login/login.component';
 import { UserLoginComponent } from './user-login/user-login.component';
