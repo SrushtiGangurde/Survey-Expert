252e56e85389998ece10d185185c492cfa1e7f7c
SrushtiGangurde <75629779+SrushtiGangurde@users.noreply.github.com>
2021-09-08 10:32:35 +0530

    view response service

--- Front End/src/app/user-home/user-home.component.ts
+++ Front End/src/app/user-home/user-home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { survey } from 'src/model/survey';
+import { LoginService } from 'src/services/login.service';
+import { ViewResponseService } from 'src/services/view-response.service';
 
 @Component({
   selector: 'app-user-home',
@@ -9,19 +11,39 @@ import { survey } from 'src/model/survey';
 
 export class UserHomeComponent implements OnInit {
 
-  surveys : survey [] = [];
+  surveys;
+  s;
+  userDet;
+  response;
+  user_id;
 
-  constructor() { }
+  constructor(private _login:LoginService,private _viewResponse: ViewResponseService) { }
 
   ngOnInit(){
-    this.initProperties();
+    this._login.getCurrentUser().subscribe(
+      (data:any)=>{
+        this.userDet=data;
+        //console.log(this.userDet);
+        this.user_id=this.userDet['user_id'];
+        
+         this.showSurveys(this.user_id)      
+      }
+    );
   }
   
-  initProperties(){
-    this.surveys=[
-      {surveyName: 'Prep up Session Survey', surveyDesc: 'Feedback for the Prep Up sessions Conducted', 
-      surveyId: 1, lastEditedBy: 5, link : 'abcsurvey', createdDate: '12/8/2021'},
-      
-    ]
-  }
+
+  showSurveys(user_id){
+    this._viewResponse.getUserSurvey(user_id).subscribe(
+      (data:any) => {
+        this.surveys = data;
+         console.log(this.surveys);
+         console.log("after service");
+  
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    }
+
 }
--- Front End/src/services/view-response.service.ts
+++ Front End/src/services/view-response.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import baseUrl from './base-url';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ViewResponseService {
 
-  constructor() { }
+  constructor(private _http:HttpClient) { }
+
+  public getUserSurvey(user_id): Observable<any>{
+    console.log("From service");
+    console.log(this._http.get(`${baseUrl}/response/user/{user_id}`,user_id));
+    return this._http.get(`${baseUrl}/response/user/{user_id}`, user_id);
+  }
 }
