d834a4b7336c09ac77ab807dd48eeca3d5d7442a
reshama-neve <reshamaneve@gmail.com>
2021-09-12 21:56:37 +0530

    login signup UI

--- Front End/src/app/survey-analysis/survey-analysis.component.ts
+++ Front End/src/app/survey-analysis/survey-analysis.component.ts
@@ -15,11 +15,10 @@ export class SurveyAnalysisComponent implements OnInit {
   ngOnInit(): void {
 
   }
-  download(){
-    this.excelService.
-    downloadFile().
-    subscribe(Blob => saveAs(Blob, "response.xlsx"))
-  }
+  // download(){
+  //   this.excelService.downloadFile().
+  //   subscribe(Blob => saveAs(Blob, "response.xlsx"))
+  // }
 
 }
 
