2fa8058fb7dafb932ea4f6b030d3abc6387321b1
SrushtiGangurde <75629779+SrushtiGangurde@users.noreply.github.com>
2021-09-12 17:38:07 +0530

    Revert "specific survey response excel file"

--- Front End/src/app/previous-survey/previous-survey.component.ts
+++ Front End/src/app/previous-survey/previous-survey.component.ts
@@ -7,7 +7,6 @@ import { ExportExcelService } from 'src/services/export-excel.service';
 import { ViewSurveysService } from 'src/services/view-surveys.service';
 import Swal from 'sweetalert2';
 import { saveAs } from 'file-saver';
-import { Survey } from '../create-survey/data-models';
 
 @Component({
   selector: 'app-previous-survey',
@@ -20,12 +19,10 @@ export class PreviousSurveyComponent implements OnInit {
     name : '',
     description :'',
   } */
-  survey_ids: any;
   surveys : any[] = [];
   public errorMsg: any;
   survey_id = 0;
   text;
-
   constructor(private _view: ViewSurveysService,
     private _route:ActivatedRoute,
     private clipboardService: ClipboardService,
@@ -39,12 +36,10 @@ export class PreviousSurveyComponent implements OnInit {
   ngOnInit() {
 
     this.survey_id=this._route.snapshot.params.sid;
-
-
     console.log("IDD");
     console.log(this.survey_id);
 
-    // alert(this.survey_id);
+    //alert(this.survey_id);
 
     this._view.getSurveys().subscribe(
       (data:any) => {
@@ -58,15 +53,7 @@ export class PreviousSurveyComponent implements OnInit {
     );
   }
 
-
-
-
-  
   copyContent(surveyid,sname) {
-
-    console.log("Inside copy",surveyid);
-
-
     this.text="http://localhost:4200/giveSurvey/"+surveyid+"/"+sname;
 
     this.clipboardService.copyFromContent(this.text)
@@ -78,34 +65,15 @@ export class PreviousSurveyComponent implements OnInit {
 
 
 
-  download(surveyid){
-    
+  download(){
+
     Swal.fire({
       title: 'File Exported!',
     })
 
-    this.excelService.generateResponseId(surveyid).subscribe(
-      (data) => {
-        console.log("Data Object ", data);
-          var k = Object.keys(data);
-
-          console.log("Array Size: ", k.length);
-
-          const responseIds=[];
-
-          for(let i = 0; i < k.length; i++){
-
-            console.log("inside generateResponseid responseid: ",data[i].response_id);
-
-            responseIds.push(data[i].response_id);
-
-          }
-          console.log("response id length : ",responseIds.length);
-
-          this.excelService.
-          downloadFile(responseIds).
-          subscribe(Blob => saveAs(Blob, "response.xlsx"))
-      },
-    );
+    this.excelService.
+    downloadFile().
+    subscribe(Blob => saveAs(Blob, "response.xlsx"))
   }
+
 }
